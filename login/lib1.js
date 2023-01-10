9d8b5484aec7fe06c00e983697271bde3537ab65
pythoonnoprog <102587740+pythoonnoprog@users.noreply.github.com>
2023-01-10 11:37:36 +0000

    cm4

--- login/lib1.js
+++ login/lib1.js
@@ -1,1 +1,14 @@
-funciton
+function next(){
+    document.getElementById("user").style.display="none";
+    document.getElementById("pass").style.display="block";
+    var user = document.getElementById("username_input").value;
+    if(user.substr(0,1) != "@"){
+       var utemp = user;
+       var user = "@"+utemp;
+       document.getElementById("un1").innerText=user;
+       document.getElementById("username_input").value=user;
+    }else{
+        document.getElementById("un1").innerText=user;
+        document.getElementById("username_input").value=user;
+    }
+}
