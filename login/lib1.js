b0de569befddc001230dee7aa9536668c31b79b1
pythoonnoprog <102587740+pythoonnoprog@users.noreply.github.com>
2023-02-04 08:59:17 +0900

    cm61

--- login/lib1.js
+++ login/lib1.js
@@ -16,11 +16,12 @@ function logins(){
     location.href="sigin.html?user="+document.getElementById("username_input").value+"&pass="+document.getElementById("ppaas").value;
 }
 window.onload = function(){
-console.log("ver 1.1.2.1");
+console.log("ver 1.1.3.1");
 if(location.search.substr(1) == "liff"){
     console.log("現在LIFFモードです。 \n 変更する場合はURLの?liffを消去してごアクセスください");
     document.getElementById("mberid").style.display="none";
-    document.getElementById("loginboxid").style="border-radius:0px;width:100%;height:100%;margin:none;padding:10px;position:absolute;top:0;left:0;";
+    document.getElementById("loginboxid").style="border-radius:0px;width:100%;height:100%;margin:none;backgound:none;";
+    document.body.style="background-repeat: no-repeat;background-image: url(https://cdn.glitch.com/e3342b72-93eb-4a4c-bdae-a0166de9f522%2F1075252.png);background-size: 100% auto;background-position:right 30% bottom 0;";
 }else if(location.search.substr(1) == "pwa"){
     console.log("現在PWAモードです。 \n 変更する場合はURLの?pwaを消去してごアクセスください");
 }else{
