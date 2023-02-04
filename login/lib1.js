899cc86d29b3c4e20fec18efc7906655b99360e6
pythoonnoprog <102587740+pythoonnoprog@users.noreply.github.com>
2023-02-04 09:01:55 +0900

    cm62

--- login/lib1.js
+++ login/lib1.js
@@ -21,7 +21,7 @@ if(location.search.substr(1) == "liff"){
     console.log("現在LIFFモードです。 \n 変更する場合はURLの?liffを消去してごアクセスください");
     document.getElementById("mberid").style.display="none";
     document.getElementById("loginboxid").style="border-radius:0px;width:100%;height:100%;margin:none;backgound:none;";
-    document.body.style="background-repeat: no-repeat;background-image: url(https://cdn.glitch.com/e3342b72-93eb-4a4c-bdae-a0166de9f522%2F1075252.png);background-size: 100% auto;background-position:right 30% bottom 0;";
+    document.body.style="position:absolute;top:0;left:0;background-repeat: no-repeat;background-image: url(https://cdn.glitch.com/e3342b72-93eb-4a4c-bdae-a0166de9f522%2F1075252.png);background-size: 100% auto;background-position:right 30% bottom 0;";
 }else if(location.search.substr(1) == "pwa"){
     console.log("現在PWAモードです。 \n 変更する場合はURLの?pwaを消去してごアクセスください");
 }else{
